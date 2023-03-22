4a1b0c96516c6290ff1d76b4a046b0b1d706dd3c
Navod <54998118+navodveduth@users.noreply.github.com>
2023-03-22 16:41:26 +0530

    admin middleware & users endpoint

--- backend/user-service/src/index.js
+++ backend/user-service/src/index.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv/config';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 
+dotenv.config();
+
+connectDB();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`.yellow.bold);
 });
