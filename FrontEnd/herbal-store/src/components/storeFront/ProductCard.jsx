dd9fc10d43f0fcec6eef5de6caf5fd550c8fe727
IT20159030 <it20159030@my.sliit.lk>
2023-03-24 16:47:59 +0530

    minor changes

--- FrontEnd/herbal-store/src/components/storeFront/ProductCard.jsx
+++ FrontEnd/herbal-store/src/components/storeFront/ProductCard.jsx
@@ -4,7 +4,7 @@ function ProductCard({ name, price, imageUrl, addToCart }) {
   return (
     <div className="max-w-xs rounded-md overflow-hidden shadow-md ">
       <img className="w-full h-48 object-cover" src={imageUrl} alt={name} />
-      <div className="bg-gray-800 text-white p-4">
+      <div className="bg-lightbg text-white p-4">
         <div className="font-semibold text-lg">{name}</div>
         <div className="text-primarylight font-bold text-xl mt-2">${price}</div>
         <button onClick={addToCart} className="bg-tertiary hover:bg-primarydark text-white font-bold py-2 px-4 rounded mt-2 w-full">
