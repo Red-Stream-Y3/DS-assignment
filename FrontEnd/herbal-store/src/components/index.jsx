9caa5e1c41165aff6fd6d4d446a9b4a8b4db7274
IT20159030 <it20159030@my.sliit.lk>
2023-03-24 16:19:02 +0530

    Created common components

---
+++ FrontEnd/herbal-store/src/components/index.jsx
@@ -0,0 +1,13 @@
+// Purpose: To export all components from one file
+// common components
+export { default as Navbar } from './common/Navbar';
+export { default as Footer } from './common/Footer';
+export { default as Hero } from './storeFront/Hero';
+
+// storeFront components
+export { default as HomeProducts } from './storeFront/HomeProducts';
+export { default as ProductCard } from './storeFront/ProductCard';
+
+// admin components
+
+// seller components
--- FrontEnd/herbal-store/tailwind.config.js
+++ FrontEnd/herbal-store/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: "#6F9940",
+        secondary: "#4C9231",
+        tertiary: "#386B25",
+        primarylight: "#7CCB5E",
+        primarydark: "#254519",
+        darkbg: "#111827"
+      },
+      width: {
+        '102': '26rem',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
