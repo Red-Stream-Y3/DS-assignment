6a807edb89703fe1d0e20cf30f40e573a2b02ac5
dinushkasam <it21047824@my.sliit.lk>
2023-03-28 12:40:46 +0530

    admin commission popup complete

--- FrontEnd/herbal-store/src/components/adminDash/AdminTopButtons.jsx
+++ FrontEnd/herbal-store/src/components/adminDash/AdminTopButtons.jsx
@@ -1,25 +1,41 @@
 import React from "react";
 
 import { AiOutlineSetting } from "react-icons/ai";
+import CommissionPopup from "./CommissionPopup";
 
 const AdminTopButtons = (props) => {
     
+        //handle click methods for commission popup 
         const handleCommissionClick = () => {
-            
+            const element = document.getElementById("adminCommissionPopup");
+            element.classList.remove("hidden");
+        }
+
+        const handleCommissionCancel = (e) => {
+            const element = document.getElementById("adminCommissionPopup");
+            const closest = e.target.closest("#adminCommissionPopupContent");
+
+            if(!closest || e.target.id === "adminCommissionPopupCancel"){
+                element.classList.add("hidden");
+            }
         }
     
+        //handle click methods for email popup
         const handleEmailClick = () => {
             
         }
     
+        //handle click methods for application settings popup
         const handleApplicationClick = () => {
             
         }
     
         return (
-            <div className=" w-fit">
+            <div className="w-fit">
                 <div className="flex">
                     <button 
+                        data-modal-target="adminCommissionModal"
+                        data-modal-toggle="adminCommissionModal"
                         className="transition-all flex w-fit p-1 bg-slate-600 rounded-md ring-1 ring-slate-400 text-slate-400 hover:bg-slate-500 hover:cursor-pointer" 
                         onClick={handleCommissionClick}>
                         <AiOutlineSetting className="inline-block m-1" />
@@ -37,6 +53,16 @@ const AdminTopButtons = (props) => {
                         <AiOutlineSetting className="inline-block m-1" />
                         <div className="mr-1">CONFIGURE APPLICATION</div>
                     </button>
+                </div>
+                <div
+                    id="adminCommissionPopup"
+                    onClick={handleCommissionCancel}
+                    className="hidden transition-all ease-in fixed left-0 top-0 right-0 z-10 w-full h-full p-4 bg-black bg-opacity-50" >
+                    <div
+                        id="adminCommissionPopupContent"
+                        className="bg-slate-700 w-fit z-20 translate-y-2/3 p-6 m-auto rounded-md">
+                        <CommissionPopup handleCancel={handleCommissionCancel} />
+                    </div>
                 </div>
             </div>
         );
---
+++ FrontEnd/herbal-store/src/components/adminDash/CommissionPopup.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const CommissionPopup = (props) => {
+    return (
+        <div className="text-slate-300">
+            <div className="text-center font-bold text-lg">
+                Order Commission
+                <hr className="opacity-50" />
+            </div>
+            <div className="my-2 text-sm">
+                Note: This commission will be applied to all orders.
+            </div>
+            <div className="my-2">
+                Current Commission: 2%
+            </div>
+            <input 
+                id="adminCommissionInput" 
+                type="number" 
+                placeholder="New commission rate"
+                className="text-slate-300 bg-slate-600 rounded-md" />
+            <div className="mt-2 w-fit m-auto">
+                <button 
+                    id="adminCommissionPopupCancel"
+                    onClick={props.handleCancel}
+                    className="transition-all mt-2 mx-5 w-20 p-1 bg-slate-600 rounded-md ring-1 ring-slate-400 text-slate-400 hover:bg-slate-500 hover:cursor-pointer">
+                    Cancel
+                </button>
+                <button
+                    className="transition-all mt-2 mx-5 w-20 p-1 bg-slate-400 rounded-md ring-1 ring-slate-600 text-slate-600 hover:bg-slate-500 hover:cursor-pointer">
+                    Confirm
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default CommissionPopup;
