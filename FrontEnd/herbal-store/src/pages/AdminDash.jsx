3bbb1d89cbafb07c2eeaccc61376c0398d0c8700
dinushkasam <it21047824@my.sliit.lk>
2023-04-04 22:27:44 +0530

    table styles updated

--- FrontEnd/herbal-store/src/pages/AdminDash.jsx
+++ FrontEnd/herbal-store/src/pages/AdminDash.jsx
@@ -15,7 +15,7 @@ const AdminDash = () => {
 
     const [orderList, setOrderList] = useState([]);
     const [userList, setUserList] = useState([]);
-    const [selectedTab, setSelectedTab] = useState("orders"); //dash, orders, users, statistics
+    const [selectedTab, setSelectedTab] = useState("dashboard"); //dash, orders, users, statistics
 
     //TODO: Admin statistics
 
@@ -40,11 +40,15 @@ const AdminDash = () => {
     const notify = (message) => toast(message);
 
     const popupBackgroundClasses = "hidden transition-all ease-in fixed left-0 top-0 right-0 z-10 w-full h-full p-4 bg-black bg-opacity-50";
-    const cardClasses = "bg-darkbg rounded-lg p-4 p-10 m-auto";
+    const cardClasses = "bg-darkbg rounded-lg px-10 py-8 m-auto";
+    const tableHeaderClasses = "sticky top-0 px-6 py-3 w-2/12 text-sm uppercase bg-gray-700 text-gray-400";
 
+    //TODO: make breadcrumb clickable
     return (
         <>
-            <div className="bg-lightbg w-full h-screen">
+            <div 
+                className="bg-lightbg w-full"
+                style={{height: "calc(100vh - 53px)"}}>
                 <Navbar />
                 <h1 id="adminBreadCrumb" className="pl-8 italic text-slate-500 my-2 transition-all">
                     {selectedTab ==="dashboard" && "Admin Dashboard"}
@@ -57,7 +61,7 @@ const AdminDash = () => {
                 <div className="flex">
                     <div 
                         className="mx-5 mb-2 w-2/12 inline-block top-56 rounded-lg bg-darkbg"
-                        style={{height: "calc(100vh - 150px)"}}
+                        style={{height: "calc(100vh - 170px)"}}
                     >
                         <AdminSidebar selector={setSelectedTab} />   
                     </div>
@@ -68,12 +72,14 @@ const AdminDash = () => {
                             {selectedTab === "dashboard" && <h1 className="text-gray-200">Satistics</h1>}
                             {selectedTab === "orders" && 
                                 <OrderList 
+                                    tableHeader={tableHeaderClasses}
                                     orders={orderList} 
                                     popupBgClasses={popupBackgroundClasses}
                                     toast={notify} />}
                             {selectedTab === "products" && <h1 className="text-gray-200">Products</h1>}
                             {selectedTab === "users" &&
                                 <UserList 
+                                    tableHeader={tableHeaderClasses}
                                     users={userList}
                                     toast={notify} />}
                             {selectedTab === "configurations" && 
