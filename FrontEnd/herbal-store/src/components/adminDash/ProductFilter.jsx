2dc2338aaa83186630ff29a0ddca7a81d1263adc
dinushkasam <it21047824@my.sliit.lk>
2023-04-09 20:54:06 +0530

    email service implemented

--- FrontEnd/herbal-store/src/components/adminDash/ProductFilter.jsx
+++ FrontEnd/herbal-store/src/components/adminDash/ProductFilter.jsx
@@ -10,12 +10,12 @@ const ProductFilter = (props) => {
         <div className="my-2 flex justify-between">
             <div className="text-2xl inline-block">Products</div>
             <input 
-                id="adminUserSearch"
+                id="adminProductSearch"
                 className="ml-10 w-72 p-1 bg-slate-600 rounded-md ring-1 ring-slate-400 text-slate-400" 
                 type="text" 
                 value={props.search}
                 onChange={handleSearchChange}
-                placeholder="Search Users" /> 
+                placeholder="Search Products" /> 
         </div>
     );
 }
---
+++ backend/email-service/src/routes/EmailRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const getGmailTransporter = require('../controllers/EmailService');
+const getAccount = require('../config/HostAccount');
+const mailGenerator = require('../controllers/MailGenerator');
+
+router.route('/send').post(async (req, res) => {
+    // Get email data from request body
+    const { to, subject, mail } = req.body;
+    const account = await getAccount();
+    const transporter = await getGmailTransporter();
+
+    //prepare content for Mailgen
+    const data = {
+        body: {
+            name: mail.header,
+            intro: mail.intro,
+            table: {
+                data: mail.tableData
+            },
+            action: {
+                instructions: mail.action.instructions,
+                button: {
+                    text: mail.action.buttonText,
+                    link: mail.action.buttonLink
+                }
+            },
+            outro: mail.outro
+        }
+    };
+
+    //generate mail html
+    const html = mailGenerator.generate(data);
+
+    // Send email
+    const result = await transporter.sendMail(
+        {
+            from: `"RedStream" <${account.user}>`,
+            to,
+            subject,
+            html,
+        },
+        (err, info) => {
+            if (err) {
+                res.status(400).send({ error: err, msg: "Email not sent!" });
+            } else {
+                res.status(200).send({ msg: "Email sent!", info });
+            }
+        }
+    );
+
+});
+
+module.exports = router;
