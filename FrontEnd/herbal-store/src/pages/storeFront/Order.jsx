ffd107b3897cb4527d45fd612378ff00cfe0a237
Navod <54998118+navodveduth@users.noreply.github.com>
2023-04-10 19:56:38 +0530

    add paypal payment

--- FrontEnd/herbal-store/src/pages/storeFront/Order.jsx
+++ FrontEnd/herbal-store/src/pages/storeFront/Order.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate, Link } from 'react-router-dom';
-import { getOrderDetails } from '../../actions/orderActions';
+import axios from 'axios';
+import { PayPalButton } from 'react-paypal-button-v2';
+import { getOrderDetails, payOrder } from '../../actions/orderActions';
+import { ORDER_PAY_RESET } from '../../constants/orderConstants';
 import { Loader, Message, Navbar } from '../../components';
 
 const Order = () => {
@@ -16,6 +19,11 @@ const Order = () => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const [paypalSdk, setPaypalSdk] = useState(false);
+
+  const orderPay = useSelector((state) => state.orderPay);
+  const { loading: loadingPay, success: successPay } = orderPay;
+
   if (!loading) {
     //   Calculate prices
     const addDecimals = (num) => {
@@ -28,10 +36,54 @@ const Order = () => {
   }
 
   useEffect(() => {
-    if (!order) {
-      dispatch(getOrderDetails(id));
+    if (!userInfo) {
+      navigate('push');
     }
-  }, [dispatch, id, navigate, order, userInfo]);
+
+    const addPayPalScript = async () => {
+      const { data: CLIENT_ID } = await axios.get(
+        'http://localhost:9124/api/config/paypal'
+      );
+
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = `https://www.paypal.com/sdk/js?client-id=${CLIENT_ID}`;
+      script.async = true;
+      script.onload = () => {
+        setPaypalSdk(true);
+      };
+      document.body.appendChild(script);
+    };
+
+    if (!order || successPay) {
+      dispatch({ type: ORDER_PAY_RESET });
+      dispatch(getOrderDetails(id));
+    } else if (!order.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript();
+      } else {
+        setPaypalSdk(true);
+      }
+    }
+  }, [dispatch, id, successPay, order, userInfo, navigate]);
+
+  const successPaymentHandler = (paymentResult) => {
+    console.log(paymentResult);
+    dispatch(payOrder(id, paymentResult));
+  };
+
+  // format date function for payment date
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day}  ${hours}:${minutes}:${seconds}`;
+  };
 
   console.log(order);
   return loading ? (
@@ -153,7 +205,7 @@ const Order = () => {
                       </div>
                     </div>
 
-                    <div className="w-2/5 pb-10 text-right pr-5">
+                    <div className="w-2/5 pb-10 text-right">
                       <span className="text-xl text-white">
                         {item.quantity} x ${item.price} = $
                         {item.quantity * item.price}
@@ -179,9 +231,108 @@ const Order = () => {
               ) : (
                 <div>
                   <h1 className="text-xl font-bold text-white pb-5">
-                    Payment Method
+                    Order Status
                   </h1>
-                  <div className="p-5 scrollbar scrollbar-thumb-primarylight scrollbar-track-lightbg overflow-y-auto border-2 border-solid border-primarylight bg-darkbg rounded-xl"></div>
+                  <div className="p-5 scrollbar scrollbar-thumb-primarylight scrollbar-track-lightbg overflow-y-auto border-2 border-solid border-primarylight bg-darkbg rounded-xl">
+                    <div className="flex justify-between py-4">
+                      {order.isDelivered ? (
+                        <div
+                          variant="danger"
+                          className="border-2 border-solid border-green-500 w-full p-2 rounded-md flex justify-between "
+                        >
+                          <div className="text-md font-medium text-white">
+                            Delivered on :
+                          </div>
+                          <div className="text-md font-medium text-green-500 pr-5">
+                            {order.deliveredAt}
+                          </div>
+                        </div>
+                      ) : (
+                        <div
+                          variant="danger"
+                          className="border-2 border-solid border-red-500 w-full p-2 rounded-md flex justify-between"
+                        >
+                          <div className="text-md font-medium text-white">
+                            Delivery :
+                          </div>
+                          <div className="text-md font-medium text-red-400 pr-5">
+                            Not Delivered
+                          </div>
+                        </div>
+                      )}
+                    </div>
+
+                    <div className="flex justify-between py-4">
+                      {order.isPaid ? (
+                        <div className="border-2 border-solid border-green-500 w-full p-2 rounded-md flex justify-between">
+                          <div className="text-md font-medium text-white">
+                            Paid on :
+                          </div>
+                          <div className="text-md font-medium text-green-500  pr-5">
+                            {formatDate(order.paidAt)}
+                          </div>
+                        </div>
+                      ) : (
+                        <div className="border-2 border-solid border-red-500 w-full p-2 rounded-md flex justify-between">
+                          <div className="text-md font-medium text-white">
+                            Payment :
+                          </div>
+                          <div className="text-md font-medium text-red-400 pr-5">
+                            Not Paid
+                          </div>
+                        </div>
+                      )}
+                    </div>
+
+                    {!order.isPaid && (
+                      <div className="px-20 mt-10">
+                        {loadingPay && <Loader />}
+                        {!paypalSdk ? (
+                          <Loader />
+                        ) : (
+                          <PayPalButton
+                            amount={order.totalPrice}
+                            onSuccess={successPaymentHandler}
+                          />
+                        )}
+                      </div>
+                    )}
+
+                    {order.isPaid && (
+                      <div className="px-20">
+                        <div class="p-6 md:mx-auto">
+                          <svg
+                            viewBox="0 0 24 24"
+                            class="text-green-500 w-16 h-16 mx-auto my-6"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
+                            ></path>
+                          </svg>
+                          <div class="text-center">
+                            <h3 class="md:text-2xl text-base text-white font-semibold text-center">
+                              Payment Done!
+                            </h3>
+                            <p class="text-white my-2">
+                              Thank you for completing your secure online
+                              payment.
+                            </p>
+                            <p className="text-white"> Have a great day! </p>
+                            <div class="py-10 text-center">
+                              <Link
+                                to="/"
+                                className="px-12 bg-secondary hover:bg-green-500 text-white hover:text-darkbg font-semibold py-3 rounded-2xl"
+                              >
+                                Continue Shopping
+                                <i className="fas fa-arrow-right ml-3"></i>
+                              </Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
               )}
             </div>
