74b51aec3fca7dcdc120639254b9d5c97a98cdfc
IT20159030 <it20159030@my.sliit.lk>
2023-04-16 13:20:05 +0530

    minor changes

--- FrontEnd/herbal-store/src/components/storeFront/UserProfile.jsx
+++ FrontEnd/herbal-store/src/components/storeFront/UserProfile.jsx
@@ -292,14 +292,13 @@ function UserProfile() {
                   Account Type
                 </label>
                 <div className="mt-2">
-                  <select
-                    id="account-type"
+                  <input 
+                    type="checkbox"
                     name="account-type"
-                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primarylight sm:max-w-xs sm:text-sm sm:leading-6"
-                  >
-                    <option selected>Buyer</option>
-                    <option>Seller</option>
-                  </select>
+                    id="account-type"
+                    className="mr-2"
+                  />
+                  <label htmlFor="account-type" className="text-sm leading-6 text-gray-400">Change my account type to seller</label>
                 </div>
               </div>
             </div>
