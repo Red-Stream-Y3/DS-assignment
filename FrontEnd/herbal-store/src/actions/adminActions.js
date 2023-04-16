631b0faa1aecfdb15ffe12d958915e7480bae463
Navod <54998118+navodveduth@users.noreply.github.com>
2023-04-16 18:16:15 +0530

    add get commission details action

--- FrontEnd/herbal-store/src/actions/adminActions.js
+++ FrontEnd/herbal-store/src/actions/adminActions.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+import {
+  COMMISSION_DETAILS_REQUEST,
+  COMMISSION_DETAILS_SUCCESS,
+  COMMISSION_DETAILS_FAIL,
+} from '../constants/adminConstants';
+import { logout } from './userActions';
 
 //get userdata from browser storage
 const user = localStorage.getItem('userInfo')
@@ -283,4 +289,45 @@ export const getMonthlyOrders = async (year, month, setter) => {
         }));
       }
     });
+};
+
+export const getCommissionDetails = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: COMMISSION_DETAILS_REQUEST,
+    });
+
+    // const {
+    //   userLogin: { userInfo },
+    // } = getState();
+
+    // const config = {
+    //   headers: {
+    //     Authorization: `Bearer ${userInfo.token}`,
+    //   },
+    // };
+
+    const { data } = await axios.get(`http://localhost:9122/v1/commission`);
+
+    // const { data } = await axios.get(`http://localhost:9122/v1/commission`);
+
+    dispatch({
+      type: COMMISSION_DETAILS_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('commission', JSON.stringify(data));
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout());
+    }
+    dispatch({
+      type: COMMISSION_DETAILS_FAIL,
+      payload: message,
+    });
+  }
 };
