1f3da79fa8a113446f9b9966b7ff6d8062c69c53
Janindu Dissanayake <87414583+JaninduDissanyake@users.noreply.github.com>
2023-04-22 11:06:34 +0530

    new components

---
+++ FrontEnd/herbal-store/src/components/sellerDash/ShopOrders.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ShopOrders() {
+  return (
+    <div className="p-1 shadow-md text-white">
+      <div 
+        className="overflow-x-auto" 
+        style={{ maxHeight: "30rem", minHeight: "20rem", }}>
+        <h1 className="text-gray-200 text-center text-2xl font-bold py-4">ShopOrders</h1>
+        
+        </div>
+        </div>
+  )
+}
---
+++ FrontEnd/herbal-store/src/components/sellerDash/ShopPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react' 
+
+export default function ShopPage() {
+  return (
+    <div className="p-1 shadow-md text-white">
+      <div 
+        className="overflow-x-auto" 
+        style={{ maxHeight: "30rem", minHeight: "20rem", }}>
+        <h1 className="text-gray-200 text-center text-2xl font-bold py-4">Shop Name</h1>
+        
+        </div>
+    </div>
+
+  )
+}
