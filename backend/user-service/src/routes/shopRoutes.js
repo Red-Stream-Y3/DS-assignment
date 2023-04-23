505fe1d41450f12a162fd6fb812e197acd8dc5e3
Navod <54998118+navodveduth@users.noreply.github.com>
2023-04-23 12:41:57 +0530

    adminSeller middleware

--- backend/user-service/src/routes/shopRoutes.js
+++ backend/user-service/src/routes/shopRoutes.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import {
-    getshopById,
-    getshops,
-    updateshop, createShop
+  getshopById,
+  getshops,
+  updateshop,
+  createShop,
 } from '../controllers/shopcontroller.js';
-import { protect, admin,  seller } from '../middleware/authMiddleware.js';
+import { protect, adminSeller } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(createShop, protect, admin, seller);
-router.route('/').get(getshops, protect, admin, seller);
-router.route('/:id').get(getshopById, protect, admin, seller).put(updateshop, protect, admin, seller);
+router.route('/').post(createShop, protect, adminSeller);
+router.route('/').get(getshops, protect, adminSeller);
+router
+  .route('/:id')
+  .get(getshopById, protect, adminSeller)
+  .put(updateshop, protect, adminSeller);
 
 export default router;
