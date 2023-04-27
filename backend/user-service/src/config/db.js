2bd16b7c2478943e10552b564b6e8aa83f63a936
IT20159030 <it20159030@my.sliit.lk>
2023-04-27 19:05:51 +0530

    rate product

--- backend/user-service/src/config/db.js
+++ backend/user-service/src/config/db.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {});
+    // db connection
 
     console.log(
       `MongoDB database connection established successfully: ${conn.connection.host}`
